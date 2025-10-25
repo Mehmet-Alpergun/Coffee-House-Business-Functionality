b0586474c98c044e47b096885cc825c3b70ca9dc
Mehmet Alpergun <mehmetalpergunn@gmail.com>
2025-10-25 17:30:05 +0300

    fix: changed the base adress for deploying

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/deployet/",
+  base: "/Coffee-House-Business-Functionality/",
   build: {
     rollupOptions: {
       input: {
