1cecba1229d0165c19eb42629b35d2619279206b
Mehmet Alpergun <mehmetalpergunn@gmail.com>
2025-10-25 17:25:04 +0300

    fix: added files for deploying

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  base: "/deployet/",
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        menu: resolve(__dirname, "menu.html"),
+        cart: resolve(__dirname, "cart.html"),
+        register: resolve(__dirname, "register.html"),
+        signin: resolve(__dirname, "signin.html"),
+      },
+    },
+  },
+});
